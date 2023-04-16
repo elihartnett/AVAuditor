b287b5deb3e65a15578c13ad4b3f202b75408b30
elichartnett <elichartnett@icloud.com>
2023-04-15 22:10:51 -0400

    Initial Commit

---
+++ MeetingMate/MeetingMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingMateApp.swift
+//  MeetingMate
+//
+//  Created by Eli Hartnett on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MeetingMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
